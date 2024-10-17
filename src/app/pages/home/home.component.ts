cd93ab1e3772b16d0098332b86528fea808edaf1
ansariisaad <saad.ansari@ariantechsolutions.com>
2024-10-17 12:13:47 +0530

    backup

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
+  imports: [CommonModule],
+    templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  isSidebarOpen = true; // Sidebar is open by default
+
+  // Function to toggle the sidebar
+  toggleSidebar(): void {
+    // debugger
+    console.log("It is not working")
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 
 }
+
