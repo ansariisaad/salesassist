954eab849bb04872d3bb1f45e9c7cbfa53cbd184
ansariisaad <saad.ansari@ariantechsolutions.com>
2024-11-04 18:09:27 +0530

    final

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://192.168.1.13:4090/api/' 
-export const dinosourUrl = 'https://dinosaur-cute-lightly.ngrok-free.app/api/'
+export const baseUrl = 'http://192.168.1.13:4090/api/';
+export const dinosourUrl = 'https://dinosaur-cute-lightly.ngrok-free.app/api/';
